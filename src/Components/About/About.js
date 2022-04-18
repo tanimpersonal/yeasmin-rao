298da56f1ee7f385eb50a2e5ff0b45cce6b9b95b
tanimpersonal <tashintanim2019@gmail.com>
2022-04-18 10:38:10 +0600

    demo about added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import yeasmin from "../../images/yeasmin rao.jpg";
 const About = () => {
-  return <div>I'm about</div>;
+  return (
+    <div className="my-5 mx-5">
+      <div className="main d-flex gap-5 justify-content-between align-items-center">
+        <div className="image w-100">
+          <img className="w-100" src={yeasmin} alt="" />
+        </div>
+        <div className="text px-5">
+          <p>
+            Born in Tehran, Iran and moved to the United States to go to college
+            and studied art, concentrating in photography in California State
+            University, Northridge. Focusing more in Fine Art photography during
+            academic years in college, but also passionate about applying it to
+            documentary photography.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
